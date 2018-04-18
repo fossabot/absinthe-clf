9e61309041e0dccf6ae3691f914b1c8483974377
Ed Johns <edwardgjohns3@gmail.com>
2018-04-18 14:33:06 -0400

    Added version for epub. Set module path in documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,7 +24,7 @@ copyright = '2018, Edward Johns'
 author = 'Edward Johns'
 
 # The short X.Y version
-version = ''
+version = '0.0'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
@@ -151,7 +151,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     (master_doc, 'AbsintheConfigurableLogicFramework', 'Absinthe Configurable Logic Framework',
-     author, 'AbsintheConfigurableLogicFramework', 'One line description of project.',
+     author, 'AbsintheConfigurableLogicFramework', 'Framework for parametric generation of soft IP cores and development toolchains.',
      'Miscellaneous'),
 ]
 
