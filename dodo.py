cdc497991d55a86f374d8f02976fc074f38bb1ac
Ed Johns <edwardgjohns3@gmail.com>
2018-04-21 19:44:26 -0400

    Moved python dependencies to requirements.txt. Removed prints that were used for debugging.

--- dodo.py
+++ dodo.py
@@ -29,6 +29,22 @@ def task_run_tests():
     'verbosity': 2
   }
 
+def create_test_report():
+  """Create HTML and PDF test reports from rst files."""
+  return {
+    'actions': [lambda : print("  not implemented.")],
+    'verbosity': 2
+  }
+
+def task_create_docs():
+  """"""
+  from doit.action import CmdAction
+
+  return {
+    'actions': [CmdAction('sphinx-build -M latexpdf source build', cwd = 'docs')],
+    'verbosity': 1
+  }
+
 if __name__ == '__main__':
     from subprocess import call
 
@@ -40,7 +56,9 @@ if __name__ == '__main__':
     print('\nInstalling Python 3 virtual environment...')
     call(['python3', '-m', 'venv', 'venv'])
     print('  Installing Python automation tools...\n')
-    call(['venv/bin/pip3', 'install', 'doit', 'wheel', 'setuptools', 'sphinx', 'sphinx-autobuild'])
+
+    # Install packages that are not simple code modules (i.e. that have executable files)
+    call(['venv/bin/pip3', 'install', '-r', 'requirements.txt'])
 
     print('\nRunning build tools...\n')
     call(["venv/bin/doit"])
